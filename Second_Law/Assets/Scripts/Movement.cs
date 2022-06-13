32f5f21d10b0c35a4c635820bd01d5055d67b1c2
Cookiedough611 <wparodi611@gmail.com>
2022-06-13 00:41:06 -0500

    troubleshooting

--- Second_Law/Assets/Scripts/Movement.cs
+++ Second_Law/Assets/Scripts/Movement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float coFriction;
+    public Rigidbody2D player; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Rigidbody2D>();
+        player.AddForce(new Vector2(2,3), ForceMode2D.Force);
     }
 
     // Update is called once per frame
