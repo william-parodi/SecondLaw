e9ce043d5c05db1e942318a2f766c037eaec44ab
rogueemu <52797089+rogueemu@users.noreply.github.com>
2022-06-15 16:42:09 -0500

    second law & reset player function

--- Second_Law/Assets/Scripts/Movement.cs
+++ Second_Law/Assets/Scripts/Movement.cs
@@ -10,11 +10,12 @@ public class Movement : MonoBehaviour
 
     Touch touch;
     Vector2 touchPosition;
-
+    Vector2 startPosition;
+    double timer = 1.00;
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 startPosition = GameObject.FindGameObjectWithTag("startPosition").transform.position;
+        startPosition = GameObject.FindGameObjectWithTag("startPosition").transform.position;
         player.freezeRotation = true;
         player.position = startPosition;
         print(player);
@@ -23,12 +24,19 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if (timer > 0){
+            timer -= Time.deltaTime;
+        }
+        if (timer < 0){
+            timer = 0;
+        }
         if (!(Pause_Menu.isPaused)){
             //Applies a force relative to the distance the click is away 
             //from the center and the angle it is from the horizontal
-            if (Input.GetMouseButtonDown(0)){
+            if (Input.GetMouseButtonDown(0) && timer == 0){
                 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 player.AddForce(touchPosition, ForceMode2D.Impulse);
+                timer = 1.00;
             }
 
         }
@@ -37,13 +45,20 @@ public class Movement : MonoBehaviour
     private void OnTriggerEnter2D (Collider2D collision){
         if (collision.tag == "endZone"){
             print("you win!");
+            resetPlayer();
         }
         else if (collision.tag == "outOfBounds") {
             gameOver();
+            resetPlayer();
         }
  
     }
     void gameOver(){
         print("game over");
+    }
+    void resetPlayer(){
+        player.velocity = Vector2.zero;
+        player.position = startPosition;
+        timer = 1;
     }
 }
